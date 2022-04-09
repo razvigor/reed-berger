f1509ac71aed0bfced3e822e2ea723c053c34db9
razvigor <kamaradsasa@gmail.com>
2022-04-09 15:00:34 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import PostList from './components/PostList';
+import Post from './components/Post';
+import CreatePost from './components/CreatePost';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className='App'>
-			<div className='App-header'>
-				<h1>Reed Berger Course</h1>
-			</div>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/post-list' element={<PostList />} />
+				<Route path='/post:id' element={<Post />} />
+				<Route path='/create-post' element={<CreatePost />} />
+			</Routes>
 		</div>
 	);
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			<ul>
+				<li>
+					<NavLink to='/'>Home</NavLink>
+				</li>
+				<li>
+					<NavLink to='/post-list'>Post List</NavLink>
+				</li>
+				<li>
+					<NavLink to='/create-post'>Create Post</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
